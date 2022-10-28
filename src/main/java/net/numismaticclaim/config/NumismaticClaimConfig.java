22b85adb35b3eff822587aacaecec15b4eb69324
Globox1997 <mitschke11@web.de>
2022-10-28 16:30:53 +0200

    fixed config name

--- src/main/java/net/numismaticclaim/config/NumismaticClaimConfig.java
+++ src/main/java/net/numismaticclaim/config/NumismaticClaimConfig.java
@@ -4,7 +4,7 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
-@Config(name = "medievalweapons")
+@Config(name = "numismaticclaim")
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class NumismaticClaimConfig implements ConfigData {
 
