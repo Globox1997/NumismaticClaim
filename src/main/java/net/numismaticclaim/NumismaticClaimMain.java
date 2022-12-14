1ec9ddf136ebfd61ada402a4f3639e9de806fc3a
Globox1997 <mitschke11@web.de>
2022-12-14 10:16:30 +0100

    added summon command

--- src/main/java/net/numismaticclaim/NumismaticClaimMain.java
+++ src/main/java/net/numismaticclaim/NumismaticClaimMain.java
@@ -3,7 +3,20 @@ package net.numismaticclaim;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.numismaticclaim.access.VillagerAccess;
 import net.numismaticclaim.config.NumismaticClaimConfig;
 import net.numismaticclaim.network.NumismaticClaimServerPacket;
 
@@ -17,6 +30,28 @@ public class NumismaticClaimMain implements ModInitializer {
         AutoConfig.register(NumismaticClaimConfig.class, JanksonConfigSerializer::new);
         CONFIG = AutoConfig.getConfigHolder(NumismaticClaimConfig.class).getConfig();
         NumismaticClaimServerPacket.init();
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) -> {
+            dispatcher.register((CommandManager.literal("summon").requires((serverCommandSource) -> {
+                return serverCommandSource.hasPermissionLevel(2);
+            })).then(CommandManager.literal("numismaticclaim:villager").then(CommandManager.argument("pos", Vec3ArgumentType.vec3()).executes((commandContext) -> {
+                return executeSummon(commandContext.getSource(), Vec3ArgumentType.getVec3(commandContext, "pos"));
+            }))));
+
+        });
+    }
+
+    private static int executeSummon(ServerCommandSource source, Vec3d pos) {
+        World world = source.getWorld();
+        VillagerEntity villagerEntity = new VillagerEntity(EntityType.VILLAGER, world);
+        villagerEntity.refreshPositionAndAngles(new BlockPos(pos), source.getPlayer().getYaw(), 0f);
+        NbtCompound nbtCompound = new NbtCompound();
+        nbtCompound.putBoolean("NumismaticClaimTrader", true);
+        villagerEntity.initialize(source.getWorld(), world.getLocalDifficulty(new BlockPos(pos)), SpawnReason.COMMAND, null, nbtCompound);
+        ((VillagerAccess) villagerEntity).setNumismaticClaimTrader(true);
+        world.spawnEntity(villagerEntity);
+        source.sendFeedback(Text.translatable("commands.summon.success", villagerEntity.getDisplayName()), true);
+        return 1;
     }
 
 }
